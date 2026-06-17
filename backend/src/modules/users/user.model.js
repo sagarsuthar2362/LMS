602a98ac068935b461ffece3a386b4ee9c44fcb9
sagarsuthar2362 <1studywithsagar2362@gmail.com>
2026-06-17 23:37:00 +0530

    user schema created

---
+++ backend/src/modules/users/user.model.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["student", "instructor"],
+      required: true,
+    },
+    avatar: {
+      type: String,
+    },
+    bio: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
