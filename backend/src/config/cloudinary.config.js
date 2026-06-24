b07f481597254fc72f427dd0ac3d896ab29acc92
sagarsuthar2362 <1studywithsagar2362@gmail.com>
2026-06-24 20:23:27 +0530

    set up cloudinary to upload images such as thumbnail,etc

---
+++ backend/src/config/cloudinary.config.js
@@ -0,0 +1,19 @@
+import { v2 as cloudinary } from "cloudinary";
+import ApiError from "../utils/ApiError";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (path) => {
+  try {
+    let result = await cloudinary.uploader.upload(path);
+    return result.secure_url;
+  } catch (error) {
+    throw new ApiError(500, "unable to upload thumbnail");
+  }
+};
+
+export default uploadOnCloudinary;
